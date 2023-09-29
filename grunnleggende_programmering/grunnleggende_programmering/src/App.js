3dc291efbcff243f570fd1125b088638d0ea45c0
Mathias Aarskog <mathias@heiigjen.com>
2023-09-29 14:18:03 +0200

    message

--- grunnleggende_programmering/grunnleggende_programmering/src/App.js
+++ grunnleggende_programmering/grunnleggende_programmering/src/App.js
@@ -197,3 +197,17 @@ function App() {
 
 export default App;
 
+
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import { BrowserRouter } from "react-router-dom"
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+)
--- grunnleggende_programmering/grunnleggende_programmering/src/Elev.js
+++ grunnleggende_programmering/grunnleggende_programmering/src/Elev.js
@@ -3,10 +3,13 @@ export default function Elev(props) {
 let name = props.name
 console.log(name);
 
+function handleClick (){
+    console.log()
+}
 
     return (
 
-        <div className='person'>
+        <div onClick={handleClick} className='person'>
             {name}
         </div>
 
