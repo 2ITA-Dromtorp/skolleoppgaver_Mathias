e62884544e64eb771c600c693b8f02a851d74023
UnderTaker-dev <mathias@heiigjen.com>
2023-12-07 11:24:14 +0100

    funker

--- grunnleggende_programmering/grunnleggende_programmering/src/klassekart.js
+++ grunnleggende_programmering/grunnleggende_programmering/src/klassekart.js
@@ -1,44 +1,37 @@
+import React, { useState } from 'react';
 import Elev from './Elev';
 
+const initialStudentNames = ["Martin", "Mathias", "Kevin", "Andreas", "Falk","Ylva", "Vanessa", "Chen", "Fridtjof", "Luz"];
+
 export default function Klassekart() {
+  const [studentNames, setStudentNames] = useState(initialStudentNames);
 
-    return (
-        <div className='container'>
+  const shuffleStudents = () => {
+    const shuffledNames = [...initialStudentNames].sort(() => Math.random() - 0.5);
+    setStudentNames(shuffledNames);
+  };
 
-      <div className='learer'>
-
-        <Elev name="Lærer"/>
-
+  return (
+    <div className='container'>
+      <div className='teacher'>
+        <Elev name="Lærer" />
       </div>
-      
       <div className='forste_rad'>
-
-      <Elev name="Martin"/>
-      <Elev name="Mathias"/>
-      <Elev name="Kevin"/>
-      <Elev name="Andreas"/>
-
+        {studentNames.slice(0, 4).map((name, index) => (
+          <Elev key={index} name={name} />
+        ))}
       </div>
-
       <div className='andre_rad'>
-
-      <Elev name="Falk"/>
-      <Elev name="Sander"/>
-      <Elev name="Ylva"/>
-      <Elev name="Vanessa"/>
-      <Elev name="Chen"/>
-
+        {studentNames.slice(4, 9).map((name, index) => (
+          <Elev key={index} name={name} />
+        ))}
       </div>
-
       <div className='tredje_rad'>
-
-      <Elev name="Fridtjof"/>
-      <Elev name="Luz"/>
-        
+        {studentNames.slice(9).map((name, index) => (
+          <Elev key={index} name={name} />
+        ))}
+        <button onClick={shuffleStudents}>Shuffle Positions</button>
       </div>
-
-
-
     </div>
-    )
+  );
 }
