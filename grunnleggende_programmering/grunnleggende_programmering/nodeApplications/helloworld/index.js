c2dcad7e19bfbac3caf6d5a9b5a9708330db0694
UnderTaker-dev <mathias@heiigjen.com>
2023-12-07 11:25:09 +0100

    banana

--- grunnleggende_programmering/grunnleggende_programmering/nodeApplications/helloworld/index.js
+++ grunnleggende_programmering/grunnleggende_programmering/nodeApplications/helloworld/index.js
@@ -1,9 +1,39 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  port: 3306,
+  password: 'root',
+  database: 'dromtorp'
+});
+
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+  connection.connect(function (err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+  
+    console.log('connected as id ' + connection.threadId);
+  });
+
+  connection.query('SELECT * from elev', function (error, results, fields) {
+    if (error) throw error;
+    console.log(JSON.stringify(results));
+    console.log('The solution is: ', results);
+    res.send(results)
+  });
+  
+  connection.end();
+
+ 
 })
 
 app.listen(port, () => {
--- grunnleggende_programmering/grunnleggende_programmering/src/App.js
+++ grunnleggende_programmering/grunnleggende_programmering/src/App.js
@@ -144,7 +144,7 @@ export default function RPSGame() {
 }
 
 */
-
+/*
 import './App.css';
 import { Route, Routes } from "react-router-dom";
 import Klassekart from './klassekart';
@@ -161,3 +161,138 @@ function App() {
 }
 
 export default App;
+*/
+
+/*
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import DogImage from './images/Dog.jpg';
+
+function App() {
+
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [users, setUsers] = useState({});
+
+  const [person, setPerson] = useState({ navn: '', alder: 0 });
+  const [hund, setHund] = useState({ navn: '', alder: 0 });
+
+  const [visHilsen, setVisHilsen] = useState(false);
+
+  const handleInputChange = (e, type) => {
+    const { name, value } = e.target;
+    type === 'person' ? setPerson({ ...person, [name]: value }) : setHund({ ...hund, [name]: value });
+  };
+
+  const handleLoginClick = () => {
+    if (users[username] === password) {
+      setLoggedIn(true);
+    } else {
+      alert('Feil brukernavn eller passord');
+    }
+  };
+
+  const handleRegisterClick = () => {
+    const newUsers = {
+      ...users,
+      [username]: password,
+    };
+    setUsers(newUsers);
+    localStorage.setItem('users', JSON.stringify(newUsers));
+    setUsername('');
+    setPassword('');
+  };
+
+  const handleSendClick = () => setVisHilsen(true);
+
+  const handleDeleteUser = (userToDelete) => {
+    const newUsers = { ...users };
+    delete newUsers[userToDelete];
+    setUsers(newUsers);
+    localStorage.setItem('users', JSON.stringify(newUsers));
+  };
+
+  const handleUpdateUser = (usernameToUpdate, newPassword) => {
+    const newUsers = {
+      ...users,
+      [usernameToUpdate]: newPassword,
+    };
+    setUsers(newUsers);
+    localStorage.setItem('users', JSON.stringify(newUsers));
+  };
+
+  useEffect(() => {
+    const savedUsers = localStorage.getItem('users');
+    if (savedUsers) {
+      setUsers(JSON.parse(savedUsers));
+    } else {
+      const adminAccount = { admin: 'admin' };
+      setUsers(adminAccount);
+      localStorage.setItem('users', JSON.stringify(adminAccount));
+    }
+  }, []);
+
+  if (!loggedIn) {
+    return (
+      <div className="login-container">
+        <input className="input-field" type="text" onChange={(e) => setUsername(e.target.value)} placeholder="Brukernavn" />
+        <input className="input-field" type="password" onChange={(e) => setPassword(e.target.value)} placeholder="Passord" />
+        <button className="login-button" onClick={handleLoginClick}>Logg inn</button>
+        <button className="register-button" onClick={handleRegisterClick}>Registrer</button>
+      </div>
+    );
+  }
+
+  if (username === 'admin') {
+    return (
+      <div className="container" style={{ backgroundImage: `url(${DogImage})`, height: '100vh' }}>
+        <h2>Administrator Dashboard</h2>
+        <h3>Brukere:</h3>
+        <ul>
+          {Object.keys(users).map((user, index) => (
+            <li key={index}>
+              {user}
+              <button onClick={() => handleDeleteUser(user)}>Slett bruker</button>
+              <button onClick={() => handleUpdateUser(user, prompt('Skriv inn nytt passord'))}>Endre passord</button>
+            </li>
+          ))}
+        </ul>
+        <button onClick={() => setLoggedIn(false)}>Logg ut</button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="container" style={{ backgroundImage: `url(${DogImage})`, height: '100vh' }}>
+      <label className="input-label">
+        Ditt navn:
+        <input className="input-field" type="text" name="navn" value={person.navn} onChange={(e) => handleInputChange(e, 'person')} />
+      </label>
+      <label className="input-label">
+        Din alder:
+        <input className="input-field" type="number" name="alder" value={person.alder} onChange={(e) => handleInputChange(e, 'person')} />
+      </label>
+      <label className="input-label">
+        Hundens navn:
+        <input className="input-field" type="text" name="navn" value={hund.navn} onChange={(e) => handleInputChange(e, 'hund')} />
+      </label>
+      <label className="input-label">
+        Hundens alder:
+        <input className="input-field" type="number" name="alder" value={hund.alder} onChange={(e) => handleInputChange(e, 'hund')} />
+      </label>
+      <button className="send-button" onClick={handleSendClick}>Send</button>
+      <button onClick={() => setLoggedIn(false)}>Logg ut</button>
+
+      {visHilsen && (
+        <p className="greeting-message">
+          Hei, {person.navn} og {hund.navn}.  alder din {person.navn} i hundeår er omtrent {person.alder * 5} år, og {hund.navn} er {hund.alder * 5} år i hundeår.
+        </p>
+      )}
+    </div>
+  );
+}
+
+export default App;
+*/
+
