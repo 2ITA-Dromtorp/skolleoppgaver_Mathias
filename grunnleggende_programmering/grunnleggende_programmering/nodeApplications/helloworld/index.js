a5601f7faeba2ef21530d170f889de0639d944d6
UnderTaker-dev <mathias@heiigjen.com>
2024-01-04 14:34:52 +0100

    a

--- grunnleggende_programmering/grunnleggende_programmering/nodeApplications/helloworld/index.js
+++ grunnleggende_programmering/grunnleggende_programmering/nodeApplications/helloworld/index.js
@@ -9,8 +9,8 @@ app.use(cors())
 var connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
-  user: 'johu',
-  password: 'dromtorp',
+  user: 'root',
+  password: 'root',
   database: 'dromtorp'
 });
 
