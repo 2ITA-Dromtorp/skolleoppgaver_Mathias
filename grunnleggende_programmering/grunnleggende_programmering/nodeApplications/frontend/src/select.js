a364bf31b05bea1d25afa382c08f6f99691bb0a7
UnderTaker-dev <mathias@heiigjen.com>
2024-01-10 13:16:31 +0100

    a

--- grunnleggende_programmering/grunnleggende_programmering/nodeApplications/frontend/src/select.js
+++ grunnleggende_programmering/grunnleggende_programmering/nodeApplications/frontend/src/select.js
@@ -11,7 +11,7 @@ export default function Select() {
 
     const getCustomersData = () => {
         axios
-            .get("http://localhost:3000/")
+            .get("http://localhost:3001/")
             .then(response => {
                 setElevData(response.data);
             })
