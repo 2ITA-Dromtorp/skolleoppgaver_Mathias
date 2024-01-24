7651c67dc8ba3e45546828bc40e8584d9de439b4
UnderTaker-dev <mathias@heiigjen.com>
2024-01-24 12:39:31 +0100

    test

--- loginapp/src/App.js
+++ loginapp/src/App.js
@@ -1,9 +1,35 @@
+import React, { useState } from 'react';
 import './App.css';
+
+function LoginForm() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Perform login logic here
+        console.log(`Username: ${username}, Password: ${password}`);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Username:
+                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+            </label>
+            <label>
+                Password:
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </label>
+            <input type="submit" value="Log In" />
+        </form>
+    );
+}
 
 function App() {
   return (
     <div className="App">
-      
+      <LoginForm />
     </div>
   );
 }
